6ab30c6e447ecca304d45ef768575a58c78e5a6f
Marwamu <marwamohamed1293@gmail.com>
2023-10-27 18:47:10 +0200

    FAQ section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-console.log("Get started");
+// FAQ Accordion 
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content')
+
+    faqContainer.addEventListener('click', (e)=>{
+       const groupHeader = e.target.closest('.faq-group-header')
+       
+       if(!groupHeader) return;
+
+       const group = groupHeader.parentElement;
+       const groupBody= group.querySelector('.faq-group-body')
+       const icon = groupHeader.querySelector('i')
+
+
+    //    Toggle Icons
+    icon.classList.toggle('fa-plus')
+    icon.classList.toggle('fa-minus')
+
+    //    Toggle Vibaility Body
+    groupBody.classList.toggle('open')
+
+    //    CLose other FAQ bodies
+    const otherGroups = faqContainer.querySelectorAll('.faq-group');
+    console.log(otherGroups)
+    otherGroups.forEach((otherGroups) =>{
+        if(otherGroups !== group){
+            const otherGroupBody = otherGroups.querySelector('.faq-group-body');
+            const otherGroupIcon = otherGroups.querySelector('i');
+
+            otherGroupBody.classList.remove('open')
+            otherGroupIcon.classList.remove('fa-minus')
+            otherGroupIcon.classList.add('fa-plus')
+        }
+    })
+
+    })
+})
